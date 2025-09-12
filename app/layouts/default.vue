67cab22e3439fe344f61e71f6bfca6ab6ca3f069
Kireenkov22 <kireenkov22@MacBook-Air-Kireenkov.local>
2025-09-12 16:49:38 +0300

    first commit2

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,14 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// ID вашего счетчика Яндекс.Метрики
+const metrikaId = 'YOUR_YANDEX_METRIKA_ID' // Замените на ваш ID счетчика
+</script>
 
 <template>
   <div>
     <CommonHeader />
     <slot />
     <CommonFooter />
+    <CommonYandexMetrika :metrika-id="metrikaId" />
   </div>
 </template>
 
